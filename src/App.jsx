82a2030ba68a9b8e1cab9f159a8c3aee1d32d7d7
rony31416 <ronymajumder1171@gmail.com>
2024-12-16 20:41:54 +0600

    project needed to be update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Page2 from "./pages/Page2";
 import Page3 from "./pages/Page3";
 import Page4 from "./pages/Page4";
 import Page5 from "./pages/Page5";
+import Page6 from "./pages/Page6";
 import Header from "./components/Header";
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         <Page3 />
         <Page4 />
         <Page5 />
+        <Page6 />
       </div>
     </>
   );
---
+++ src/pages/Page6.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import SwiperSliders from '../components/SwiperSliders.jsx'; // Adjust the path as needed
+
+const Page6 = () => {
+  return (
+    <div className='h-screen relative p-4 bg-white'>
+    <div className='h-full w-full bg-black overflow-hidden rounded-[16px]'>
+       
+    </div>
+  </div>
+  )
+}
+
+export default Page6
